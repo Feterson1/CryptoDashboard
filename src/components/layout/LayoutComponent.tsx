11981870f907d6e5dab2dbdea7a6df9937c9a3e8
Feterson <feterson@bk.ru>
2023-05-09 23:51:11 +0600

    added sidebar display logic

--- src/components/layout/LayoutComponent.tsx
+++ src/components/layout/LayoutComponent.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { iLayout } from "../common/types/layout";
 import TopBarComponent from "../top-bar/topBar";
+import { useLocation } from "react-router-dom";
 
 const LayoutComponent = ({children}: iLayout) => {
+
+    const location = useLocation();
+
     return(
-       <>
-       <TopBarComponent/>
-       {children}
-       </>
+
+        location.pathname === '/login' || location.pathname === '/register' ?
+        (<>
+            
+            {children}
+            </>
+            ) : (
+                <>
+                <TopBarComponent/>
+                {children}
+                </>
+            )
+    
     )
 
 }
