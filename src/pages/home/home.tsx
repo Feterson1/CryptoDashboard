f4a64d7a43bce300af80bd68557527a8ff6d1856
Feterson <feterson@bk.ru>
2023-05-23 21:02:34 +0600

    get crypto currency

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useAppDispatch, useAppSelector } from "../../utils/hook";
+import { getFavoriteAssets } from "../../store/thunks/assets";
 
 
 
 const Home = () =>{
+    const favoriteAssets = useAppSelector(state => state.assets.favoriteAssets);
+    const dispatch = useAppDispatch();
+    const fetchDataRef = useRef(false);
+
+    const favoriteAssetName = ['bitcoin','ethereum'];
+
+    const fetchData = (data:string[]) =>{
+        
+        data.forEach((element: string) =>{
+
+                
+            dispatch(getFavoriteAssets(element));
+
+        }
+        )
+
+    }
+
+    console.log('assets', favoriteAssets)
+
+    useEffect(()=>{
+        if(fetchDataRef.current === true) return
+
+        fetchDataRef.current = true;
+        fetchData(favoriteAssetName);
+        
+
+    },[])
+
     return (
         <div>
             <h1>This is Home Page</h1>
