cd0250b1a9471c1914e381961e0a30bf0e618656
Feterson <feterson@bk.ru>
2023-06-03 14:46:09 +0600

    Implement WatchList logic

--- src/pages/watchlist/watchlist.tsx
+++ src/pages/watchlist/watchlist.tsx
@@ -1,11 +1,50 @@
-import react from 'react';
+import react, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../utils/hook';
+import { getWatchlistElements } from '../../store/thunks/watchlist/watchlist';
+import { getTopPriceData } from '../../store/thunks/assets';
+import AssetsTableComponent from '../../components/assets-table/assetsTable';
+import { Grid, Typography } from '@mui/material';
+import { useStyles } from './styles';
+
 
 
 const WatchListComponent = () =>{
+    const WatchList = useAppSelector((state) => state.watchList.assets);
+    const dispatch = useAppDispatch();
+    const {assets} = useAppSelector(state => state.assets);
+
+    const classes = useStyles();
+
+    console.log(assets)
+
+    useEffect(()=>{
+        dispatch(getTopPriceData());
+        dispatch(getWatchlistElements());
+    },[dispatch]);
+
+
+   const filteredArray = assets.filter((element: any)  => {
+    return WatchList.some((otherElement: any )=> {
+        return otherElement.assetId === element.id;
+    })
+
+    })
+
     return(
-        <div>
-        <h1>WatchList</h1>
-    </div>
+        <Grid className={classes.root}>
+            <Grid className={classes.watchlistHeading}>
+                <Typography 
+                variant='h2'
+                className={classes.heading}>
+                    Избранное
+                </Typography>
+
+            </Grid>
+            <Grid className={classes.assetsTableBlock}>
+            <AssetsTableComponent assets = {filteredArray}/>
+            </Grid>
+        </Grid>
+       
     )
 }
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  authSlice  from "./slice/auth/auth";
 import  assetSlice  from "./slice/assets";
+import  watchListSlice  from "./slice/watchlist/watchlist";
 
 const store = configureStore({
     reducer: {
         auth: authSlice,
         assets: assetSlice,
+        watchList: watchListSlice,
     }
 })
 
